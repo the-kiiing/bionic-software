e38116c37433f61b488ffceba73f52f350382888
the-kiiing <158551184+the-kiiing@users.noreply.github.com>
2024-02-02 12:23:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Tb from "./Tb";
-import Ab from "./Ab";
 import Navbar from "./Navbar";
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Tb />} />
-        <Route path="/ab" element={<Ab />} />
       </Routes>
     </div>
   );
